a0fb85ca8ea5e085517dcfa5e46d7593ee75e044
sandipkukadiya <sandipkukadiya@gmail.com>
2023-09-27 18:29:55 +0530

    update 11

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -24,9 +24,19 @@ class RegisterController extends Controller
     {
         $token = "6432932571:AAEiMdF3P7zigjt9rdHw2_KWLRNgDeUyXB8";
 
-        echo '<pre>';
-        print_r($request->all());
-        exit;
+        $password = rand('1111','9999').rand('1111','9999');
+        User::create([
+            'name' => $request->lastname.' '.$request->firstname,
+            'username' => $request->id,
+            'email' => $request->email,
+            'email_verify_status' => 1,
+            'password' => bcrypt($password),
+            'profile_pics' => (!empty($request->photo_url) ? $request->photo_url : ""),
+            'city'=>'jp',
+            'role'=>'user'
+        ]);
+
+        return redirect()->route('user.register')->with('success', __('Save Changes'));
 
     }
 
