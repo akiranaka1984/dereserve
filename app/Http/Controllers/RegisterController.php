959dae6cf697449179423dccc904d3c0ee959492
sandipkukadiya <sandipkukadiya@gmail.com>
2023-09-27 09:28:29 +0530

    update 4

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -22,9 +22,23 @@ class RegisterController extends Controller
 
     public function save(Request $request)
     {
-        echo '<pre>';
-        print_r($request->all());
-        exit;
+        $token = "6432932571:AAEiMdF3P7zigjt9rdHw2_KWLRNgDeUyXB8";
+        if ($_POST['auth_date'] && $_POST['hash']) {
+            $data_check = $_POST['auth_date'] . $_POST['first_name'] . $_POST['id'] . $_POST['photo_url'] . $_POST['username'];
+            $secret_key = hash('sha256', $token, true);
+            $hash = hash_hmac('sha256', $data_check, $secret_key);
+            if ($hash === $_POST['hash']) {
+                $user_id = $_POST['id'];
+                $phone_number = $_POST['phone'];
+                
+                echo '<pre>';
+                print_r($phone_number);
+                exit;
+
+            }
+        }
+
+
     }
 
 }
